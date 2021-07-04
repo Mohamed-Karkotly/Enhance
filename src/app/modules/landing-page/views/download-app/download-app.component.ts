c3696d5bfcd0f0537ac771f43fc844d8f15e5cc0
Mohamed.Karkotly <karkotly.work@gmail.com>
2021-07-04 04:10:13 +0300

    add download app feature

--- src/app/modules/landing-page/views/download-app/download-app.component.ts
+++ src/app/modules/landing-page/views/download-app/download-app.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-download-app',
   templateUrl: './download-app.component.html',
-  styleUrls: ['./download-app.component.scss']
+  styleUrls: ['./download-app.component.scss'],
 })
 export class DownloadAppComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  downloadApp() {
+    const link = document.createElement('a');
+    link.setAttribute(
+      'href',
+      'https://store8.gofile.io/download/ca2197ee-05b5-44cf-a3d8-b19576a9fbe6/Enhance-App.apk'
+    );
+    link.setAttribute('download', `enhance`);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
   }
-
 }
